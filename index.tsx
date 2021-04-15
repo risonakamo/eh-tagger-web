6f90153ece3eea0264d5bac35ab8817e872d6024
risonakamo <hmykanae@gmail.com>
2021-04-14 22:37:49 -0400

    beginning initial design

--- index.tsx
+++ index.tsx
@@ -6,7 +6,18 @@ import "css/index.less";
 function IndexMain():JSX.Element
 {
   return <>
-    hello
+    <div className="tagger-entries">
+      <div className="tagger-entry">
+        test entry 1
+      </div>
+      <div className="tagger-entry">
+        test entry 2
+      </div>
+    </div>
+
+    <div className="tagger-box">
+
+    </div>
   </>;
 }
 
